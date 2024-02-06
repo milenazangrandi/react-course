24763ce8679484deb0bf67b3f3bc555724d6e916
milenazangrandi <milenazangrandi@gmail.com>
2024-02-06 20:43:43 +0000

    Quiz Application using useReducer Hook

---
+++ 10-react-quiz/starter/react-quiz/src/components/NextButton.js
@@ -0,0 +1,21 @@
+export default function NextButton({ dispatch, index, numQuestions }) {
+  console.log(index, numQuestions);
+  if (index < numQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "nextQuestion" })}
+      >
+        Next
+      </button>
+    );
+  if (index === numQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "finish" })}
+      >
+        Finish
+      </button>
+    );
+}
