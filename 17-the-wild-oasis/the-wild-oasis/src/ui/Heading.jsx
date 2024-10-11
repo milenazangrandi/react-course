8140adb92518d23ca2d7d7f777675eb6bb7089e2
Milena Zangrandi <Milena@192.168.1.105>
2024-10-11 03:54:17 +0100

    Authentication with Supabase

--- 17-the-wild-oasis/the-wild-oasis/src/ui/Heading.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/ui/Heading.jsx
@@ -1,24 +1,32 @@
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components';
 
 const Heading = styled.h1`
+    ${(props) =>
+        props.as === 'h1' &&
+        css`
+            font-size: 3rem;
+            font-weight: 600px;
+        `}
+    ${(props) =>
+        props.as === 'h2' &&
+        css`
+            font-size: 2rem;
+            font-weight: 600px;
+        `}
   ${(props) =>
-    props.as === "h1" &&
-    css`
-      font-size: 3rem;
-      font-weight: 600px;
-    `}
+        props.as === 'h3' &&
+        css`
+            font-size: 2rem;
+            font-weight: 500px;
+        `}
+
   ${(props) =>
-    props.as === "h2" &&
-    css`
-      font-size: 2rem;
-      font-weight: 600px;
-    `}
-  ${(props) =>
-    props.as === "h3" &&
-    css`
-      font-size: 2rem;
-      font-weight: 500px;
-    `}
+        props.as === 'h4' &&
+        css`
+            font-size: 3rem;
+            font-weight: 600px;
+            text-align: center;
+        `}
 `;
 
 export default Heading;
